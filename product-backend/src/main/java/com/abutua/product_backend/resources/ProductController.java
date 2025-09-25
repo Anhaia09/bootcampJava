de039bb1609aa8ff2f54a69d848ac5c76d67af68
Anhaia09 <leticia.anhaia09@gmail.com>
2025-09-24 22:45:11 -0300

    refactor: refactoring the product endpoint

--- product-backend/src/main/java/com/abutua/product_backend/resources/ProductController.java
+++ product-backend/src/main/java/com/abutua/product_backend/resources/ProductController.java
@@ -1,25 +1,53 @@
 package com.abutua.product_backend.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.abutua.product_backend.models.Product;
 
+import jakarta.annotation.PostConstruct;
+
 @RestController
 public class ProductController {
-    
-    @GetMapping("product")
-    public Product getProduct(){
 
-        //Criando um objeto produto
-        Product p = new Product();
+    private List<Product> products = new ArrayList<>();
 
-        p.setId(1);
-        p.setName("Notebook");
-        p.setPrice(2000);
+    @PostConstruct
+    public void init() {
 
-        return p;
+        Product p1 = new Product();
+        p1.setId(1);
+        p1.setName("Mouse");
+        p1.setPrice(100.00);
+
+        Product p2 = new Product();
+        p2.setId(2);
+        p2.setName("Capinha");
+        p2.setPrice(20.00);
+
+        Product p3 = new Product();
+        p3.setId(3);
+        p3.setName("Carregador");
+        p3.setPrice(25.00);
+
+        products.add(p1);
+        products.add(p2);
+        products.add(p3);
+
     }
 
+    @GetMapping("products/{id}")
+    public Product getProduct(@PathVariable int id) {
+        return products.get(id - 1);
+    }
+
+    @GetMapping("products")
+    public List<Product> geProducts() {
+        return products;
+    }
 
 }
